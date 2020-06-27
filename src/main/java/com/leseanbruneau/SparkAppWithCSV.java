6989b61384c220864b24c599fc2846f602c9c1e1
bel <bel@bel-VirtualBox>
2020-06-27 08:17:21 -0500

    Add a couple SparkSQL functions

--- src/main/java/com/leseanbruneau/SparkAppWithCSV.java
+++ src/main/java/com/leseanbruneau/SparkAppWithCSV.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.*;
 
 
 public class SparkAppWithCSV {
@@ -27,7 +28,29 @@ public class SparkAppWithCSV {
 		
 		horseRacesDF.printSchema();
 		horseRacesDF.show();
+		
+		// Apache Spark Extra Stuff - use if you like
+		//
+		// Create Dataframe with less columns
+		
+		horseRacesDF = horseRacesDF.drop("DATE").drop("SDAY")
+								.drop("FIN1").drop("PRGNUM1").drop("PPNUM1").drop("FODDS1").drop("PTFAV1")
+								.drop("FIN2").drop("PRGNUM2").drop("PPNUM2").drop("FODDS2").drop("PTFAV2")
+								.drop("FIN3").drop("PRGNUM3").drop("PPNUM3").drop("FODDS3").drop("PTFAV3")
+								.drop("FIN4").drop("PRGNUM4").drop("PPNUM4").drop("FODDS4").drop("PTFAV4");
 
+		Dataset<Row> turfRacesDF = horseRacesDF.filter("upper(SURFACE) like 'TURF' ");
+		
+		System.out.println("Show the Turf races");
+		turfRacesDF.show();
+		
+		Dataset<Row> highDollarRacesDF = horseRacesDF.filter("PURSE >= 80000 ");
+		
+		highDollarRacesDF = highDollarRacesDF.orderBy(col("PURSE").desc());
+		
+		System.out.println("Show races with purse money greater than or equal to 80,000, sorted by purse money in decending order");
+		highDollarRacesDF.show();
+		
 	}
 
 }
